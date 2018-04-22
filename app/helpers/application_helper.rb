97eb8c428533349c45009e2cb0bde7e101a7218f
Akshay <kp.akshay10@gmail.com>
2018-04-22 14:18:03 +0530

    Final Upload

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,24 @@
+module ApplicationHelper
+  def alert_for(flash_type)
+  { success: 'alert-success',
+    error: 'alert-danger',
+    alert: 'alert-warning',
+    notice: 'alert-info'
+  }[flash_type.to_sym] || flash_type.to_s
+end
+def form_image_select(post)
+ return image_tag post.image.url(:medium),
+                  id: 'image-preview',
+                  class: 'img-responsive' if post.image.exists?
+ image_tag 'big_thumb_169e65a7f12181ff2af17591f73bf8d8.jpg', id: 'image-preview', class: 'img-responsive'
+end
+
+def profile_avatar_select(user)
+  return image_tag user.avatar.url(:medium),
+                   id: 'image-preview',
+                   class: 'img-responsive img-circle profile-image' if user.avatar.exists?
+  image_tag 'default-avatar.jpg', id: 'image-preview',
+                                  class: 'img-responsive img-circle profile-image'
+end  
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+class User < ActiveRecord::Base
+  acts_as_voter
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :user_name, presence: true, length: { minimum: 4, maximum: 16 }
+  has_attached_file :avatar, styles: { medium: '152x152#' }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  has_many :follower_relationships, foreign_key: :following_id, class_name: 'Follow'
+  has_many :followers, through: :follower_relationships, source: :follower
+
+  has_many :following_relationships, foreign_key: :follower_id, class_name: 'Follow'
+  has_many :following, through: :following_relationships, source: :following
+
+  def follow(user_id)  
+    following_relationships.create(following_id: user_id)
+  end
+
+  def unfollow(user_id)
+    following_relationships.find_by(following_id: user_id).destroy
+  end
+
+end
